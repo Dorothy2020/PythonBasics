0a1ce946b7690d1e80d2de13186243d6b46b1044
Dorothy2020 <wangarraakoth@gmail.com>
2022-11-24 13:07:32 +0300

    PythonBasics

---
+++ input.py
@@ -0,0 +1,11 @@
+birth_year=int(input("Birth Year:"))
+print(birth_year)
+
+# String formating
+x="Dorothy"
+y="My name is {}"
+print(y.format(x))
+
+a=20
+b=20
+print(f"He said his age is {2*(a+b)}")
