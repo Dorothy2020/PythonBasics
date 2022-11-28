52cbd1da4e0928f4e397861fa926ffa315990675
Dorothy2020 <wangarraakoth@gmail.com>
2022-11-28 07:49:33 +0300

    added conditions

---
+++ class.py
@@ -0,0 +1,6 @@
+class Cow:
+    def eat(self):
+        print("eat")
+
+        Cow1=Cow()
+        Cow1.eat()
--- input.py
+++ input.py
@@ -9,3 +9,19 @@ print(y.format(x))
 a=20
 b=20
 print(f"He said his age is {2*(a+b)}")
+
+
+price=10
+print("price")
+
+
+
+# if...Statements
+
+number=4.6
+if number>0:
+    print(number,"is positive number")
+
+number=-4
+if number>0:
+    print(number,"is a positive number")
